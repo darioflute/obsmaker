c1fccc548e93616c71e2ab18de9289dcdf80e783
Dario Fadda <darioflute@gmail.com>
2019-10-11 15:56:44 -0700

    prepare conda version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ config = {
     'name': 'obsmaker',
     'version': _info['version'],
     'description': 'FIFI-LS AOR maker',
-    'long_description': 'Program to make FIFI-LS AORs',
+    'long_description': 'Program to make scan description files from AORs for FIFI-LS observations',
     'author': 'Dario Fadda',
     'author_email': 'darioflute@gmail.com',
     'url': 'https://github.com/darioflute/obsmaker.git',
@@ -21,7 +21,7 @@ config = {
     'scripts': ['bin/obsmaker'],
     'include_package_data':True,
     'package_data':{'obsmaker':['version.json','data/*']},
-    'install_requires': ['numpy', 'matplotlib', 'astropy'],
+    'install_requires': ['numpy', 'pandas'],
     'classifiers':[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: GPLv3+ License",
